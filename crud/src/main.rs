d318cfe5bbd386b5b79b3c77a8f1583ba86ac0f7
udyansachdev1 <143528446+udyansachdev1@users.noreply.github.com>
2023-10-25 18:34:52 +0000

    changes

--- crud/src/main.rs
+++ crud/src/main.rs
@@ -1,53 +1,65 @@
+extern crate csv;
 extern crate rusqlite;
+
+use csv::Reader;
 use rusqlite::{Connection, Result};
 use std::env;
-use std::io;
+use std::error::Error;
+use std::fs::File;
 use std::process;
+use std::io;
+use std::io::Write;
 
-fn main() -> Result<()> {
-    // Get the SQLite database file path from a command-line argument
+fn main() -> Result<(), Box<dyn Error>> {
+    // Get the SQLite database file path and CSV file path from command-line arguments
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        eprintln!("Usage: {} <database_file>", args[0]);
+    if args.len() < 3 {
+        eprintln!("Usage: {} <database_file> <csv_file>", args[0]);
         process::exit(1);
     }
 
     let db_path = &args[1];
+    let csv_file_path = &args[2];
+
+    // Open the SQLite database connection
     let conn = Connection::open(db_path)?;
 
-    loop {
-        // Read and execute SQL queries from the command line
-        let mut input = String::new();
-        println!("Enter an SQL query (or 'exit' to quit):");
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read input");
+    // Create a table to store the CSV data
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS data (column1 TEXT, column2 TEXT, column3 TEXT)",
+        [],
+    )?;
 
-        if input.trim() == "exit" {
+    // Open the CSV file and insert its data into the SQLite database
+    let file = File::open(csv_file_path)?;
+    let mut rdr = Reader::from_reader(file);
+
+    for result in rdr.records() {
+        let record = result?;
+        conn.execute(
+            "INSERT INTO data (column1, column2, column3) VALUES (?1, ?2, ?3)",
+            &[&record[0], &record[1], &record[2]],
+        )?;
+    }
+
+    println!("CSV data imported into the SQLite database.");
+
+    // Allow SQL queries from the terminal
+    loop {
+        let mut input = String::new();
+        print!("Enter an SQL query (or 'exit' to quit): ");
+        io::stdout().flush().expect("Failed to flush stdout");
+
+        io::stdin().read_line(&mut input)?;
+
+        let query = input.trim();
+        if query == "exit" {
             break;
         }
 
-        match conn.execute_batch(&input) {
-            Ok(_) => {
-                // Print the result of the query
-                if input.to_lowercase().contains("select") {
-                    let mut stmt = conn.prepare(&input)?;
-                    let rows = stmt.query_map([], |row| {
-                        let result: String = row.get(0)?;
-                        Ok(result)
-                    })?;
-
-                    for row in rows {
-                        if let Ok(result) = row {
-                            println!("Result: {}", result);
-                        }
-                    }
-                }
-                println!("Query executed successfully.");
-            }
-            Err(err) => {
-                eprintln!("Error executing query: {:?}", err);
-            }
+        match conn.execute_batch(query) {
+            Ok(_) => println!("Query executed successfully."),
+            Err(err) => eprintln!("Error executing query: {}", err),
         }
     }
 
